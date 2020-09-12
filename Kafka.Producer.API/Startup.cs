3751f4b5aae8b6ea0aa3c4bc2dccfb4001039e6a
Jonathan Vieira Menezes <jonathanm@me.local>
2020-09-11 22:52:35 -0300

    initial commit

---
+++ Kafka.Producer.API/Startup.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Kafka.Producer.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(opts =>
+            {
+                opts.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Kafka",
+                    Description = "Using Kafka",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Grupo 3",
+                    }
+                });
+                //opts.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                //{
+                //    In = ParameterLocation.Header,
+                //    Description = "Por favor insira um JWT com \"Bearer\" nesse campo",
+                //    Name = "Authorization",
+                //    Type = SecuritySchemeType.ApiKey
+                //});
+
+                //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                //opts.IncludeXmlComments(xmlPath);
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "Caixa API v1.0");
+            });
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Kafka
+Exemplo da utilização do Kakfa com Asp Net Core
+
+A solução está subindo múltiplos projetos para que seja possível visualizar o funcionamento do trafego de dados no Kafka
+Temos a API que será o PRODUCER(Kafka.Producer.API) e o CONSUMER(Kafka.Consumer.Handler) que está logando os tópicos de mensagens recebidas via API no "Output"
